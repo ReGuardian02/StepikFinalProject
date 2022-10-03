168044e0779df2053da612abee2a818613a9a10d
ReGuardian <danilasuhoparov@yandex.ru>
2022-10-03 21:21:27 +0300

    gitignore addition

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_addtocart_button(browser):
+def test_addtocart_button_is_visible(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(5)
     button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')

dcd95d4f5440295b72757494aaf5db24c447d7e9
ReGuardian <danilasuhoparov@yandex.ru>
2022-10-03 19:03:38 +0300

    Create test_main_page.py

---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+def test_addtocart_button(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(5)
+    button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    assert button is not None
